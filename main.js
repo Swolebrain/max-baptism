5662af0eba67262e47c1b94bb6194d5926913078
Swolebrain <Vmoreno@columbushs.com>
2016-11-05 12:53:16 -0400

    feat: added form validation

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ $("#btn1").click(function(){
 })
 $("#btn2").click(function(){
   $("#btn2").hide();
+  var name = $("#name").val();
+  var guests = $("#numguests").val();
+  if (!name || !guests){
+    return alert("Please fill out all fields.");
+  }
   $("#status").html("Sending..")
   $.ajax({
     url: "http://fvi-grad.com:4004/email",
@@ -17,7 +22,7 @@ $("#btn2").click(function(){
     data:{
       dest_email: "marizelle.rodriguez@gmail.com",
       subject: "Reservation for Max's baptism",
-      message: "Reservation by: "+$("#name").val()+ "\nNum guests: " +$("#numguests").val(),
+      message: "Reservation by: "+ name + "\nNum guests: " + guests,
       email: "blah@blah.com",
       full_name: $("#name").val(),
       ddate: "November 27, 2016",
